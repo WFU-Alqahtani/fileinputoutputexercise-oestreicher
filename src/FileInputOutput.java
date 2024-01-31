55594224e70b85a3dfbf628d9c4a795c62c53f86
jonnykateharron <harrjk22@wfu.edu>
2024-01-31 16:37:30 -0500

    Lab completed. harron and oestreicher

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -19,32 +19,60 @@ public class FileInputOutput {
             System.out.println("Could not open input file - ending program");
             System.exit(1);
         }
-
         Scanner fileReader = new Scanner(myFile);
 
+
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter name for output file: ");
+        String fileName = kb.nextLine();
+        PrintWriter fileWriter;
+        FileOutputStream myFile2 = null;
+        try {
+            myFile2 = new FileOutputStream(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be opened for output- closing program");
+            System.exit(1);
+        } // ends catch
+        fileWriter = new PrintWriter(myFile2);
+
+        if (fileReader.hasNextLine()) {
+            fileReader.nextLine();
+        }
         while (fileReader.hasNextLine()) {
             String data = fileReader.nextLine();
-            System.out.println(data);
-
+            String[] year = data.split(",");
+            try {
+                 Integer.parseInt(year[4]);
+                fileWriter.println(year[4]);
+            }
+            catch (NumberFormatException ex) {
+                fileWriter.println("N/A");
+            }
         }
-
-        fileReader.close();
-
-
-        FileInputStream myFile2 = null;
-        try {
-            myFile2 = new FileInputStream("src/moviesDataset.csv");
-        } catch (FileNotFoundException e) {
-            System.out.println("Could not open input file - ending program");
-            System.exit(1);
-        }
-
-        Scanner fileReader2 = new Scanner(myFile2);
-        while (fileReader2.hasNextLine()) {
-            String data2 = fileReader2.nextLine();
-            String[] movie = data2.split(",");
-            System.out.println(movie[2] + " ");
-        }
+        fileWriter.close();
         fileReader.close();
     }
 }
+//
+//        }
+//
+//        fileReader.close();
+//
+//
+//       FileInputStream myFile2 = null;
+//       try {
+//            myFile2 = new FileInputStream("src/moviesDataset.csv");
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Could not open input file - ending program");
+//           System.exit(1);
+//       }
+//
+//       Scanner fileReader2 = new Scanner(myFile2);
+//        while (fileReader2.hasNextLine()) {
+//           String data2 = fileReader2.nextLine();
+//            String[] movie = data2.split(",");
+//            System.out.println(movie[2] + " ");
+//        }
+//        fileReader.close();
+//    }
+//}
