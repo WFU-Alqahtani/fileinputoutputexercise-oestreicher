c7bd698b797812974a0b5323cdab87ebf3ccfe4e
caitlinsofia <oestcs22@wfu.edu>
2024-01-31 12:04:12 -0500

    code added caitlin oestreicher & jonny Kate

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,6 +1,9 @@
+import java.io.*;
+import java.util.Scanner;
 public class FileInputOutput {
 
     public static void main(String[] args) {
+
 //       1) Open the given file inside src called moviesDataSet.csv
 //       2) Read each row in the dataset
 //       3) Print each row to the screen
@@ -9,6 +12,39 @@ public class FileInputOutput {
 //       6) Close the files after finishing the processing
 //       7) Commit your code and push it to the GitHub
 
-    }
+        FileInputStream myFile = null;
+        try {
+            myFile = new FileInputStream("src/moviesDataset.csv");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open input file - ending program");
+            System.exit(1);
+        }
 
+        Scanner fileReader = new Scanner(myFile);
+
+        while (fileReader.hasNextLine()) {
+            String data = fileReader.nextLine();
+            System.out.println(data);
+
+        }
+
+        fileReader.close();
+
+
+        FileInputStream myFile2 = null;
+        try {
+            myFile2 = new FileInputStream("src/moviesDataset.csv");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open input file - ending program");
+            System.exit(1);
+        }
+
+        Scanner fileReader2 = new Scanner(myFile2);
+        while (fileReader2.hasNextLine()) {
+            String data2 = fileReader2.nextLine();
+            String[] movie = data2.split(",");
+            System.out.println(movie[2] + " ");
+        }
+        fileReader.close();
+    }
 }
